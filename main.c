142eedae0fb578979d68931f26d76f9d5d79df8f
Ted Marozzi <edwardmarozzi@gmail.com>
2023-12-22 10:46:50 +1100

    Add necessary header files and refactor vector functions

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct Vector {
   int size;
@@ -7,7 +9,7 @@ typedef struct Vector {
   int *array;
 } vector;
 
-vector *create_vec() {
+vector *vec_create() {
   int capacity = 1;
 
   vector *vector = malloc(sizeof(*vector) + sizeof(int) * capacity);
@@ -19,34 +21,55 @@ vector *create_vec() {
   return vector;
 }
 
-void free_vec(vector *vector) {
+void vec_free(vector *vector) {
   free(vector->array);
   free(vector);
 }
 
-// TODO handle multi char numbers
-char *string_from_vec(vector *vector) {
-  int number_of_chars_per_element = 3;
-
-  int number_of_chars = vector->size * number_of_chars_per_element;
-  char *string = malloc(sizeof(char) * number_of_chars);
-
-  for (size_t i = 0; i + 1 < number_of_chars;
-       i += number_of_chars_per_element) {
-    string[i] = vector->array[i / number_of_chars_per_element] + '0';
-
-    if (i + number_of_chars_per_element <= number_of_chars - 1) {
-      string[i + 1] = ',';
-      string[i + 2] = ' ';
-    }
+int vec_get_item(vector *vector, int index) {
+  if (index >= vector->size) {
+    printf("Index out of bounds\n");
+    exit(1);
   }
+  return vector->array[index];
+}
 
-  string[number_of_chars - 1] = '\0';
+void vec_set_item(vector *vector, int index, int item) {
+  if (index >= vector->size) {
+    printf("Index out of bounds\n");
+    exit(1);
+  }
+  vector->array[index] = item;
+}
+
+// TODO create a dynamic string class and use here instead of huge buffer
+char *vec_to_string(vector *vector) {
+  int buffer_size = 10000;
+  char *string = (char *)malloc(sizeof(char) * buffer_size);
+
+  for (size_t i = 0; i < vector->size; i++) {
+    int formatting_count = 4;
+    int digit_count = 11;
+    int null_terminator_count = 1;
+
+    int max_number_chars =
+        formatting_count + digit_count + null_terminator_count;
+
+    char number[max_number_chars];
+    if (i == 0) {
+      sprintf(number, "{ %d, ", vec_get_item(vector, i));
+    } else if (i < vector->size - 1) {
+      sprintf(number, "%d, ", vec_get_item(vector, i));
+    } else {
+      sprintf(number, "%d }", vec_get_item(vector, i));
+    }
+    strcat(string, number);
+  }
 
   return string;
 }
 
-void append_vec(vector *vector, int value) {
+void vec_append(vector *vector, int item) {
   vector->size = vector->size + 1;
 
   if (vector->size > vector->capacity) {
@@ -55,14 +78,14 @@ void append_vec(vector *vector, int value) {
         (int *)realloc(vector->array, sizeof(int) * vector->capacity);
   }
 
-  vector->array[vector->size - 1] = value;
+  vector->array[vector->size - 1] = item;
 }
 
-void append_array_vec(vector *vector, int values[], size_t values_size) {
+void vec_append_array(vector *vector, int items[], size_t items_size) {
 
   int original_size = vector->size;
 
-  vector->size = original_size + values_size;
+  vector->size = original_size + items_size;
 
   while (vector->size > vector->capacity) {
     vector->capacity *= 2;
@@ -70,25 +93,39 @@ void append_array_vec(vector *vector, int values[], size_t values_size) {
         (int *)realloc(vector->array, sizeof(int) * vector->capacity);
   }
 
-  for (size_t i = 0; i < values_size; i++) {
+  for (size_t i = 0; i < items_size; i++) {
+    vec_set_item(vector, original_size + i, items[i]);
+  }
+}
 
-    vector->array[original_size + i] = values[i];
+void vec_append_vec(vector *vector, int items[], size_t items_size) {
+
+  int original_size = vector->size;
+
+  vector->size = original_size + items_size;
+
+  while (vector->size > vector->capacity) {
+    vector->capacity *= 2;
+    vector->array =
+        (int *)realloc(vector->array, sizeof(int) * vector->capacity);
+  }
+
+  for (size_t i = 0; i < items_size; i++) {
+    vec_set_item(vector, original_size + i, items[i]);
   }
 }
 
 int main(void) {
-  vector *vector = create_vec();
+  vector *vector = vec_create();
 
-  append_vec(vector, 8);
+  int items[] = {INT_MIN, INT_MAX, 2};
+  vec_append_array(vector, items, sizeof(items) / sizeof(int));
+  vec_append(vector, 1);
 
-  int values[] = {4, 3, 2, 1};
-  append_array_vec(vector, values, sizeof(values) / sizeof(values[0]));
-  append_vec(vector, 1);
-
-  char *string = string_from_vec(vector);
+  char *string = vec_to_string(vector);
   printf("Vector: %s\n", string);
 
-  free_vec(vector);
+  vec_free(vector);
   free(string);
   return 0;
 }
