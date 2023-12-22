d1a8046dba2d7c8d8ec13b675a6af4d826247cf4
Ted Marozzi <edwardmarozzi@gmail.com>
2023-12-23 09:00:11 +1100

    Fix string append function to correctly null-terminate the string

--- src/string.c
+++ src/string.c
@@ -30,7 +30,7 @@ void string_append_chars(String *string, char *chars) {
   int chars_size = strlen(chars);
   int original_size = string->length;
 
-  string->length = original_size + chars_size;
+  string->length = original_size + chars_size + 1;
 
   while (string->length > string->capacity) {
     string->capacity *= 2;
@@ -45,6 +45,8 @@ void string_append_chars(String *string, char *chars) {
   for (size_t i = 0; i < chars_size; i++) {
     string->chars[original_size + i] = chars[i];
   }
+
+  // TODO: Invalid write of size 1
   string->chars[string->length] = '\0';
 }
 
