1448762e195a6732e8e1cb6d691709d5228c0552
Ted Marozzi <edwardmarozzi@gmail.com>
2023-12-23 22:26:08 +1100

    Fix missing null termination in string_append_chars function

--- src/string.c
+++ src/string.c
@@ -30,7 +30,7 @@ void string_append_chars(String *string, char *chars) {
   int chars_size = strlen(chars);
   int original_size = string->length;
 
-  string->length = original_size + chars_size + 1;
+  string->length = original_size + chars_size;
 
   while (string->length > string->capacity) {
     string->capacity *= 2;
@@ -45,9 +45,6 @@ void string_append_chars(String *string, char *chars) {
   for (size_t i = 0; i < chars_size; i++) {
     string->chars[original_size + i] = chars[i];
   }
-
-  // TODO: Invalid write of size 1
-  string->chars[string->length] = '\0';
 }
 
 void string_append_string(String *string, String *string_two) {
